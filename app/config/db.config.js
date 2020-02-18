ed7f3cfeaed8358db5f805eba4dab90f468080d6
eagler <fucheronkala@gmail.com>
2020-02-17 23:22:02 -0600

    Update db.config.js
    Actualizacion de credenciales

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e24378878xxx",
-  PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  HOST: "us-cdbr-iron-east-04.cleardb.net",
+  USER: "bb8b9fb76f4362",
+  PASSWORD: "ac693f8a",
+  DB: "heroku_2606e019cdf0e13"
 };
